fc73ea13d2d0281db24dc6503594a03f115bdc06
Ningrui Li <ningrui.li@duke.edu>
2015-01-14 12:35:10 -0500

    added plot title input argument to centertrace as well as more input error checking

--- README.md
+++ README.md
@@ -13,4 +13,7 @@ The `field` folder contains the code used to run the Field II simulations for co
 ## Metrics
 The `metrics` folder contains functions for creating various plots of each intensity field for convenient comparison between Field II and KZK simulation results.
 
+## Comparisons
+The `comparisons` folder contains the scripts used to load and compare the intensity data for the Field II and KZK sims. The functions from `metrics` will be used to compare these intensity fields.
+
 ningrui.li@duke.edu
--- metrics/centertrace.m
+++ metrics/centertrace.m
@@ -1,44 +1,55 @@
-function [] = centertrace(OutputName, depth, varargin)
-% function [] = centertrace(OutputName, intensity1, i1_depth, i1_name, intensity2, i2_depth, i2_name, ...)
+function [] = centertrace(OutputName, PlotTitle, varargin)
+% function [] = centertrace(OutputName, intensity1, int1_depth, int1_name, intensity2, int2_depth, int2_name, ...)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % INPUT:
-% OutputName - file name of output center trace plot
+% OutputName (String) - file name of output center trace plot
+% PlotTitle (String) - title of plot 
 % intensity1 - 3D intensity field following conventions used in fem tools where
 % axial extends into -z (with z = 0 at transducer face), lateral extends
 % into +y, and elev extends into -x. Assume first dimension is elevation, 
 % second dimension is lateral, and the third dimension is depth.
-% i1_depth - vector of depth values (cm) at which intensity values are
+% int1_depth - vector of depth values (cm) at which intensity values are
 % calculated (ex. 0:-0.5:-30)
-% i1_name (String) - name of intensity field (ex. 'KZK Sim C5-2 30mm Focus')
+% int1_name (String) - name of intensity field (ex. 'KZK Sim C5-2 30mm Focus')
 %
 % There can be a variable number of intensity inputs, as long as all of the
 % intensity inputs contain the 3D intensity matrix, the vector of depth
 % values, and the name (in that order).
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % OUTPUT:
-% PNG of plot is saved to CWD with name OutputName.
+% PNG file depicting plot is saved to CWD with file name OutputName.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % EXAMPLE:
-% centertrace('intensity_centertrace.png', kzk_intensity, kzk_depth, 'KZK
-% Sim, C5-2, 30 mm Focus', field_intensity, field_depth, 'Field II Sim,
-% 'C5-2', 30 mm Focus')
+% centertrace('intensity_centertrace.png', 'Intensity vs. Depth', kzk_intensity,
+% kzk_depth, 'KZK Sim, C5-2, 30 mm Focus', field_intensity, field_depth, 
+% 'Field II Sim, 'C5-2', 30 mm Focus')
 
 if ~ischar(OutputName)
-    error('OutputName must be a string.\n')
+    error('OutputName must be a string.')
+end
+
+if ~ischar(PlotTitle)
+    error('PlotTitle must be a string.')
 end
 
 if mod(length(varargin), 3) ~= 0
-    error(strcat('Each intensity field input requires three components: the', ...
-         ' intensity matrix, the depth vector, and the intensity field name'))
+    error(['Each intensity field input requires three components: the ', ...
+         'intensity matrix, the depth vector, and the intensity field name'])
 end
 
 for i = 1:3:length(varargin)
     if size(varargin{i}, 3) ~= length(varargin{i+1})
-        error('Depth vector not same length as depth dimension in intensity field input.')
+        error(['Depth vector not same length as depth dimension in ', ...
+             sprintf('intensity field input %d.', i)])
     end
 end
 
-
+%plot(depth, 
 
 end
+
+
+
+
+
 
