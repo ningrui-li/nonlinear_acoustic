b7c83fbd54e93121c45c300078953e6eaffb1182
Ningrui Li <ningrui.li@duke.edu>
2015-01-28 11:28:49 -0500

    changed to absolute filepath names for saving and loading pressure data

--- face_pressure/focus30mm/run_create_qsymm_30mm.m
+++ face_pressure/focus30mm/run_create_qsymm_30mm.m
@@ -1,8 +1,7 @@
 clear; clf;
-
-load pressure_waveforms_median_untilted.mat
+load /luscinia/nl91/lab/nonlinearity/focus70mm/pressure_waveforms_median_untilted.mat
 
 pressure = createQuarterSymmetry(pressure);
 pressure = single(pressure);
 
-save pressure_waveforms_median_qsymmetric_single_precision_interpolated_axes.mat
+save /luscinia/nl91/lab/nonlinearity/focus70mm/pressure_waveforms_median_qsymmetric_single_precision_interpolated_axes.mat
