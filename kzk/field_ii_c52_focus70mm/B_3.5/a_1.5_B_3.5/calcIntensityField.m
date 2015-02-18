0c524c773e1380f45188b83f0f3557b17133df51
Ningrui Li <ningrui.li@duke.edu>
2015-02-18 10:49:35 -0500

    updated .gitignore to ignore sge output files. also added directories for kzk sims run w/ different attenuation (alpha) and nonlinear coefficients (beta)

---
+++ kzk/field_ii_c52_focus70mm/B_3.5/a_1.5_B_3.5/calcIntensityField.m
@@ -0,0 +1,33 @@
+%% Kristina's method for calculating intensity planes
+addpath /luscinia/kah67/Nonlinear_Sims/3DSims/3d_functions
+addpath /luscinia/kah67/102113_Verasonics_measurements/routines
+addpath ../intensity_field_functions/
+load /luscinia/kah67/102113_Verasonics_measurements/20131018/focus70/11pwr/p0p0mm/pressure_waveforms_median
+load /luscinia/nl91/nonlinear_acoustic/kzk/field_ii_c52_focus70mm/depthVals.mat
+
+
+nX = size(lat,1);
+nY = size(ele,1);
+nTau = size(t,2);
+
+rho0 = 1030;
+c0 = 1540;
+
+nRun=sizeOfFile('P.dat')/(nX*nY*nTau*8)
+
+intensity = zeros(nRun, nX, nY);
+
+for n=1:1:nRun
+  fprintf('%d out of %d\n', n, nRun)
+  pout=readGenoutSlice('P.dat',n-1,nX*nY*nTau);   
+  tmp = reshape(pout,nTau,nX,nY);
+  max_wave = c52wave(tmp,t);
+  psquare = (1/size(max_wave,1)).*sum((max_wave.^2)./(rho0*c0).*(1/100).^2);
+  psquare = squeeze(psquare);
+  intensity(n,:,:)= psquare;
+ %maxPressSquared(n) = max(psquare(round(nX/2), round(nY/2)));
+end
+
+depth = depthStep;
+
+save c52_70mm_intensity_field_field_ii_pressure_input.mat intensity lat ele depth nRun
