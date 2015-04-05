1a8df3a5382e623a9b9ec3b98eb4ab6864bfc0e6
Ningrui Li <ningrui.li@duke.edu>
2015-04-05 19:26:26 -0400

    added code for running field ii sim with attenuation = 0.005 dB/cm/MHz

---
+++ field/field_c52_70mm/a_0.005/run_field_c52_70mm.m
@@ -0,0 +1,6 @@
+clear; clf;
+
+fem_root = '/luscinia/nl91/matlab/fem';
+addpath(fullfile(fem_root, 'field'));
+addpath /luscinia/nl91/matlab/Field_II/
+field2dyna('nodes.dyn', 0.005, 3.5, [0 0 0.07], 2.36, 'c52', 'gaussian')
