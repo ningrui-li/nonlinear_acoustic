cb105f08ceb0250ce7d834128ba0588f8490a963
Ningrui Li <ningrui.li@duke.edu>
2015-04-05 19:01:52 -0400

    added field ii 70mm sim readme

--- field/field_c52_70mm/run_field_c52_70mm.m
+++
@@ -1,6 +0,0 @@
-clear; clf;
-
-fem_root = '/luscinia/nl91/matlab/fem';
-addpath(fullfile(fem_root, 'field'));
-addpath /luscinia/nl91/matlab/Field_II/
-field2dyna('nodes.dyn', 0.45, 3.5, [0 0 0.07], 2.36, 'c52', 'gaussian')
