2d2c20da9d1324037f01020c5d0c61eea3c63010
Ningrui Li <ningrui.li@duke.edu>
2015-04-21 15:00:48 -0400

    updated kzk_make_loads into a function

--- dyna/loads/kzk_make_loads.m
+++ dyna/loads/kzk_make_loads.m
@@ -8,16 +8,12 @@ function [] = kzk_make_loads(intensityFile, eleVol)
 % eleVol - volume of each element (cm^3)
 
 %% load data stuff. add path stuff.
-addpath /luscinia/nl91/matlab/fem/field
-addpath /luscinia/nl91/scratch/c52/focus30mm/original/
-load dyna-kzk-f2.36-F2.6-FD0.030-a0.45.mat
+addpath /home/mlp6/git/fem/field/
 
+load(intensityFile)
 %% create inputs to makeLoadsTemps
 % use max(intensity) as IsppaNorm (not sure if this is right)
 IsppaNorm = max(intensity);
-
-% name of fake dyna file
-intensityFile = 'dyna-kzk-f2.36-F2.6-FD0.030-a0.45.mat'; 
 
 % pulse duration from /luscinia/kah67/102113_Verasonics_measurements/...
 % 20131018/focus30/11pwr/p0p0mm/pressure_waveforms_median
@@ -28,6 +24,8 @@ pulseDuration = 20; % microseconds
 % http://en.wikipedia.org/wiki/Heat_capacity#Table_of_specific_heat_capacities
 cv = 4.1760;        % (J/cm^3/C)   
 
+% scale down intensities
+%intensity = intensity;
 %% call to makeLoadsTemps
 makeLoadsTemps(intensityFile, intensityFile, IsppaNorm, pulseDuration,...
                cv, eleVol, 'q', 1);
