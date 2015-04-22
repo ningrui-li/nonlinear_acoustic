f8184fe45b58c2bf40b7d8c56a5f0d344c43420d
Ningrui Li <ningrui.li@duke.edu>
2015-04-22 15:52:56 -0400

    added focal depth input arg to calc_sws

--- sws/calc_sws.m
+++ sws/calc_sws.m
@@ -1,4 +1,4 @@
-function [sws] = calc_sws(alpha, beta, depth, printPlot)
+function [sws] = calc_sws(focus, alpha, beta, depth, printPlot)
 %% calc_sws.m
 % Estimates shear wave speed at a particular depth using the time-to-peak
 % algorithm.
@@ -11,7 +11,7 @@ if nargin < 4
     printPlot = 0;
 end
     
-eval(sprintf('load /pisgah/nl91/scratch/data/E3.0kPa/foc70mm/B_%.1f/a_%.3f/F3.5/EXCDUR_300us/res_sim.mat', beta, alpha));
+eval(sprintf('load /pisgah/nl91/scratch/data/E3.0kPa/foc%dmm/B_%.1f/a_%.3f/F3.5/EXCDUR_300us/res_sim.mat', focus, beta, alpha));
 
 lat_start = 2;           % lateral position start index for SWS estimation
 lat_end = length(lat)-4; % lateral position end index for SWS estimation
@@ -36,14 +36,14 @@ sws = linear_regression(1);
 
 if printPlot
     figure
-    plot(ttp*1e3, lat, 'k-') % note conversion from s to ms for time to peak
+    plot(ttp*1e3, lat*1e3, 'k-') % note conversion from s to ms for time to peak
     xlabel('Time to Peak (ms)', 'FontSize', 14)
     ylabel('Lateral Tracking Position (mm)', 'FontSize', 14)
     title(sprintf(['SWS Estimation at Depth of %.1f mm', nln, ...
-                   '(\\alpha=%.3f dB/cm/MHz, \\beta=%.1f)'],...
-                   axial(depth), alpha, beta), 'FontSize', 18)
+                   '(focal depth=%d mm, \\alpha=%.3f dB/cm/MHz, \\beta=%.1f)'],...
+                   axial(depth), focus, alpha, beta), 'FontSize', 18)
                
-    eval(sprintf('print -dpng sws_ttp_a%.3f_B%.1f_depth%.1f.png', alpha, beta, axial(depth)))
+    eval(sprintf('print -dpng sws_ttp_foc%d_a%.3f_B%.1f_depth%.1f.png', focus, alpha, beta, axial(depth)))
 end
 
 
