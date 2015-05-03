54d1666f3197074e40ba062256ca0f2232ce25e1
Ningrui Li <ningrui.li@duke.edu>
2015-05-03 19:15:15 -0400

    added additional input args to kzk_to_dyna and updated make_loads_script according with new input args

--- dyna/loads/kzk_to_dyna.m
+++ dyna/loads/kzk_to_dyna.m
@@ -1,5 +1,12 @@
-function [] = kzk_to_dyna(intensity_file, attenuation)
-
+function [] = kzk_to_dyna(intensity_file, attenuation, latLim, eleLim, axialLim, Fnum, focus)
+%% 
+% INPUTS:
+% intensity_file - name of intensity file
+% attenuation - attenuation coefficient (dB/cm/MHz)
+% latLim - absolute value of max lateral position of dyna sim
+% eleLim - absolute value of max elevational position of dyna sim
+% Fnum - F/# of intensity sim (focal depth / aperture width)
+% focus - focal depth (axial position) of transducer in mm
 load(intensity_file) 
 
 %% pre-processing (unit conversion + change to Field II coordinates, etc)
@@ -7,12 +14,15 @@ load(intensity_file)
 % elevation, depth, lateral positions
 intensity = permute(intensity, [3 2 1]);
 
-intensity = intensity((find(ele==-6):find(ele==0)), ...
-                      (find(lat==0):find(lat==20)), ...
+% make quarter symmetric (can be commented out if we have quarter symmetric
+% data, but should still work).
+intensity = intensity((find(ele==-eleLim):find(ele==0)), ...
+                      (find(lat==0):find(lat==latLim)), ...
                       :);
+                  
 % conversion from mm to cm
-lat = lat(find(lat==0):find(lat==20))/10;
-ele = ele(find(ele==-6):find(ele==0))/10;
+lat = lat(find(lat==0):find(lat==latLim))/10;
+ele = ele(find(ele==-eleLim):find(ele==0))/10;
 depth = depth/10;
 % making axial dimension negative w/ transducer face at z = 0
 depth = -depth;
@@ -35,16 +45,19 @@ end
 FIELD_PARAMS.measurementPoints = FIELD_PARAMS.measurementPointsandNodes(:, 2:4);
 
 %% interpolating intensity values to correspond to a mesh of uniform size
-latDynaMesh = 0:0.02:2;
-eleDynaMesh = -.6:0.02:0;
-depthDynaMesh = -9:0.025:0;
+latEleSpacing = 0.02;
+depthSpacing = 0.025;
+
+latDynaMesh = 0:latEleSpacing:(latLim/10);
+eleDynaMesh = -(eleLim/10):latEleSpacing:0;
+depthDynaMesh = -axialLim:depthSpacing:0;
 
 [eleGridOrig, latGridOrig, depthGridOrig] = ndgrid(ele, lat, depth);
 [eleGrid, latGrid, depthGrid] = ndgrid(eleDynaMesh, latDynaMesh, depthDynaMesh);
 intensity_interp = interpn(eleGridOrig, latGridOrig, depthGridOrig, intensity, eleGrid, latGrid, depthGrid, 'linear');
-% lose the first and last depth planes due to those depth values being
-% outside interpolation range, so we just replace those planes with the
-% non-interpolated data.
+% usually we lose the first and last depth planes due to those depth values being
+% outside interpolation range, so we just replace those undefined depth planes 
+% with the first and last depth planes in the non-interpolated data.
 intensity_interp(:, :, 1) = intensity(:, :, 1);
 intensity_interp(:, :, end) = intensity(:, :, end);
 % next step is to match up intensity values with measurementPointsAndNodes
@@ -62,11 +75,11 @@ end
 %% other field parameters (mostly hard-coded for c52)
 FIELD_PARAMS.Frequency = 2.36;           % MHz
 FIELD_PARAMS.Transducer = 'c52';    
-FIELD_PARAMS.alpha = attenuation;               % dB/cm/MHz
-FIELD_PARAMS.Fnum = 3.5;     
+FIELD_PARAMS.alpha = attenuation;        % dB/cm/MHz
+FIELD_PARAMS.Fnum = Fnum;     
 FIELD_PARAMS.soundSpeed = 1540;          % m/s
 FIELD_PARAMS.samplingFrequency = 5e9/10; % Hz (?) from try3d_kzk_sim.m
-FIELD_PARAMS.focus = [0 0 0.070];        % m
+FIELD_PARAMS.focus = [0 0 focus/1e3];    % m
 FIELD_PARAMS.Impulse = 'gaussian';       % won't be used
 %% save to dyna file
 %eval(sprintf('save dyna-kzk-f%.2f-F%.1f-FD%.3f-a%.2f.mat intensity FIELD_PARAMS',...
--- dyna/loads/make_loads_script.m
+++ dyna/loads/make_loads_script.m
@@ -8,10 +8,15 @@ element_vol = .02*.02*.025;
 a = {'0.005', '0.3', '0.45', '1.0', '1.5'};
 B = {'0', '3.5', '7'};
 
-cd focus70mm
-
 addpath /luscinia/nl91/nonlinear_acoustic/dyna/loads/
 
+%% 70 mm focal depth
+cd focus70mm
+axialLim = 52;   % mm
+eleLim = 6;      % mm
+latLim = 25;     % mm
+Fnum = 3.5;    
+focalDepth = 70; % mm
 for beta = B
     % go into each nonlinear coefficient (B) subdirectory
     eval(sprintf('cd B_%s', char(beta)));
@@ -24,8 +29,45 @@ for beta = B
         % make pseudo-dyna*.mat file
         eval(sprintf(['kzk_to_dyna(''/luscinia/nl91/nonlinear_acoustic/kzk/',...
                  'field_ii_c52_focus70mm/B_%s/a_%s_B_%s/',...
-                 'c52_70mm_intensity_field_field_ii_pressure_input.mat'', %f)'],...
-                 char(beta), char(alpha), char(beta), str2num(char(alpha))))
+                 'c52_70mm_intensity_field_field_ii_pressure_input.mat'', %f, %d, %d, %d, %.1f, %d)'],...
+                 char(beta), char(alpha), char(beta), str2num(char(alpha)), latLim, eleLim, axialLim, Fnum, focalDepth))
+             
+        % call the kzk_make_loads function to make PointLoads file
+        eval(sprintf(['kzk_make_loads(''dyna-kzk.mat'', %f)'],...
+                 element_vol))
+   
+        cd ..
+    end
+    cd ..
+end
+
+cd ..
+
+%% 30 mm focal depth
+a = {'0.005'};
+B = {'0', '3.5', '7'};
+axialLim = 52;   % mm
+eleLim = 6;      % mm
+latLim = 12;     % mm
+Fnum = 2.6;    
+focalDepth = 30; % mm
+
+cd focus30mm
+
+for beta = B
+    % go into each nonlinear coefficient (B) subdirectory
+    eval(sprintf('cd B_%.1f', str2num(char(beta))));
+    
+    for alpha = a
+        % go into each attenuation coefficient (a) subdirectory
+        eval(sprintf('cd a_%.3f', str2num(char(alpha))));
+        
+        % call the kzk_to_dyna file to make interpolate intensity field and
+        % make pseudo-dyna*.mat file
+        eval(sprintf(['kzk_to_dyna(''/luscinia/nl91/nonlinear_acoustic/kzk/',...
+                 'field_ii_c52_focus30mm/B_%s/a_%s_B_%s/',...
+                 'c52_30mm_intensity_field_field_ii_pressure_input.mat'', %f, %d, %d, %d, %.1f, %d)'],...
+                 char(beta), char(alpha), char(beta), str2num(char(alpha)), latLim, eleLim, axialLim, Fnum, focalDepth))
              
         % call the kzk_make_loads function to make PointLoads file
         eval(sprintf(['kzk_make_loads(''dyna-kzk.mat'', %f)'],...
